20e4fd2a9359122124246072ec1f8523ebc9c165
ZeRedBarron <paulinobarron85@outlook.com>
2023-10-12 08:58:47 -0700

    Reorganization fix

---
+++ javascript/images.js
@@ -0,0 +1,138 @@
+//
+
+var loaded = false;
+
+//I'm using Derek Leung Image Loader method: https://jsfiddle.net/user/DerekL
+
+//ImageCollection class
+function ImageCollection(list) {
+	this.list = list;
+	this.total = 0;
+	this.images = {};
+	this.config(list);
+}
+ImageCollection.prototype.config = function(list){
+	var total = this.total
+	for(var i = 0; i < list.length; i++) {
+		var img = new Image();
+		this.images[list[i].name] = img;
+		window.addEventListener("load", function(){
+            total++;
+            if(total === list.length){
+				loaded = true;
+            }
+        });
+		img.src = list[i].url;
+	}
+}
+ImageCollection.prototype.get = function(name){
+	return this.images[name];
+}
+ImageCollection.prototype.waitToRun = function(){
+	return new Promise(function(resolve, reject){
+		if(loaded) {
+			resolve();
+		}
+		else {
+			window.addEventListener("load", function(){
+				resolve();
+			});
+		}
+		if(reject) {
+			window.addEventListener("error", function(){
+				throw new Error("Image fetching failed");
+			});
+			
+		}
+	});
+}
+
+
+var imgs = new ImageCollection([
+	{
+		name: "MoonBackground",
+		url: "Moon Background 2.png"
+	},
+	{
+		name: "BlockTexture",
+		url: "graphics/block_01.png"
+	},
+	{
+		name: "BlockTexture2",
+		url: "graphics/block_02.png"
+	},
+	{
+		name: "BlockTexture3",
+		url: "graphics/block_03.png"
+	},
+	{
+		name: "BlockTexture4",
+		url: "graphics/block_04.png"
+	},
+	{
+		name: "BlockTexture5",
+		url: "graphics/block_05.png"
+	},
+	{
+		name: "BlockTexture6",
+		url: "graphics/block_06.png"
+	},
+	{
+		name: "BlockTexture7",
+		url: "graphics/block_07.png"
+	},
+	{
+		name: "BlockTexture8",
+		url: "graphics/block_08.png"
+	},
+	{
+		name: "BlockTexture9",
+		url: "graphics/block_09.png"
+	},
+	{
+		name: "BlockTexture10",
+		url: "graphics/block_10.png"
+	},
+	{
+		name: "BlockTexture11",
+		url: "graphics/block_11.png"
+	},
+	{
+		name: "BlockTexture12",
+		url: "block_12.png"
+	},
+	{
+		name: "WoodTexture",
+		url: "graphics/wood_block_01.png",
+	},
+  {
+    name: "WoodTexture2",
+    url: "graphics/wood_block_02.png",
+  },
+  {
+    name: "WoodTexture3",
+    url: "graphics/wood_block_03.png",
+  },
+  {
+    name: "WoodTexture4",
+    url: "graphics/wood_block_04.png",
+  },
+  {
+    name: "WoodTexture5",
+    url: "graphics/wood_block_05.png",
+  },
+  {
+    name: "PlayerTexture",
+    url: "player_block.png",
+  }
+  ,
+  {
+    name: "PlayerTexture2",
+    url: "graphics/player_block_02.png",
+  },
+	{
+		name: "blank",
+		url: "blank_01.png"
+	}
+]);
+
---
+++ javascript/particles.js
@@ -0,0 +1,73 @@
+
+//Particle Object
+function Particle(x, y, speed, sze, color, angle, life, height, mode) {
+	this.x = x;
+	this.y = y;
+	this.prevX = x;
+	this.prevY = y;
+	this.gravity = -height;
+	this.originalGrav = -height;
+	this.w = sze;
+	this.h = sze;
+	this.col = color;
+	this.size = sze;
+	this.speed = speed;
+	this.angle = angle;
+	this.life = life;
+	this.startLife = 0;
+	this.dead = false;
+	this.rot = 0;
+	this.startLifeValue = this.life;
+	this.mode = mode;
+}
+Particle.prototype.draw = function() {
+	ctx.save();
+	ctx.translate(this.x, this.y);
+	
+	ctx.fillStyle = "rgba(" + this.col[0] + "," + this.col[1] + "," + this.col[2] + "," + this.life/this.startLifeValue + ")"
+	ctx.beginPath();
+	ctx.ellipse(0, 0, this.size/2, this.size/2, 0, 0, Math.PI*8);
+	ctx.fill();
+	ctx.closePath();
+	ctx.restore();
+};
+Particle.prototype.updateFly = function() {
+	this.x += Math.cos(this.angle * Math.PI/180) * this.speed;
+	this.y += Math.sin(this.angle * Math.PI/180) * this.speed;
+	
+	this.rot++;
+	
+	this.life--;
+	if(this.life < 0) {
+		this.dead = true;
+	}
+};
+Particle.prototype.updateFall = function() {
+	this.prevX = this.x;
+    this.prevY = this.y;
+
+    this.gravity += 0.18;
+    this.y += this.gravity;
+
+	for(var i in blocks) {
+        if(collideHalf(this, blocks[i])) {
+            this.gravity = this.originalGrav / 2;
+            this.y = (this.prevY < blocks[i].prevY) ? blocks[i].y - this.h/2 - blockSize/2: blocks[i].y + blockSize/2 + this.h/2;
+			this.originalGrav /= 2;
+        }
+    }
+
+	/*for(var i in blocks){
+		if(collide(this, blocks[i])){
+			this.x = (this.prevX < blocks[i].prevX) ? blocks[i].x - this.w : blocks[i].x + blocks[i].h;
+		}
+	}*/
+
+	this.x += random(-2, 1);
+	
+	this.life--;
+	if(this.life < 0) {
+		this.dead = true;
+	}
+};
+
